e172291cbd8f1d40fc896c49714d2967984cc096
Cheese Kimbap <lalalajoligs@gmail.com>
2021-01-03 21:45:15 +0800

    Initial Files for Tanala
    initial codes

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<footer class="sticky-bottom">
+	<div class="copy-right">
+		<div class="container">
+
+			<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+				<ul>
+					<li><a class="facebook" href="https://bongo.cat/"><span>Facebook</span></a></li>
+					<li><a class="twitter" href="https://bongo.cat/"><span>Twitter</span></a></li>
+				</ul>
+			</div>
+			<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2020 TANALA . Brought To You By <a href="https://code-projects.org/">RoadToPICC </a></p>
+		</div>
+	</div>
+</footer>
---
+++ package-list.php
@@ -0,0 +1,125 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(!isset($_GET['range'])){
+	$sql = "SELECT * from tbltourpackages";
+}
+if(isset($_GET['range'])){
+	$range = $_GET['range'];
+	$sql = "SELECT * FROM tbltourpackages WHERE PackagePrice <= $range";
+}
+
+if(!isset($_GET['features'])){
+	$sql = "SELECT * from tbltourpackages";
+}
+if(isset($_GET['features'])){
+	$features = $_GET['features'];
+	$sql = "SELECT * FROM `tbltourpackages` WHERE PackageFetures LIKE '%$features%'";
+}
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TANALA</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<?php include('includes/header.php');?>
+<!--- banner ---->
+<div class="banner-3">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> Package List</h1>
+	</div>
+</div>
+<!--- /banner ---->
+
+<div class="rooms">
+	<div class="container">
+
+		<div class="room-bottom">
+		<form method="GET" class="form-group">
+			<div class="form-group">
+				<input type="type" name="range" placeholder="Budget" class="form-control" />
+			</div>
+			<button class="btn btn-success btn-block" type="submit" name="find">Search</button>
+		</form>
+
+		<form method="GET" class="form-group">
+			<div class="form-group">
+				<input type="type" name="features" placeholder="Features" class="form-control" />
+			</div>
+			<button class="btn btn-success btn-block" type="submit" name="find">Search</button>
+		</form>
+
+			<h3>Package List</h3>
+
+
+<?php 
+$query = $dbh->prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{	?>
+			<div class="rom-btm">
+				<div class="col-md-3 room-left wow fadeInLeft animated" data-wow-delay=".5s">
+					<img src="admin/pacakgeimages/<?php echo htmlentities($result->PackageImage);?>" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-6 room-midle wow fadeInUp animated" data-wow-delay=".5s">
+					<h4>Package Name: <?php echo htmlentities($result->PackageName);?></h4>
+					<h6>Package Type : <?php echo htmlentities($result->PackageType);?></h6>
+					<p><b>Package Location :</b> <?php echo htmlentities($result->PackageLocation);?></p>
+					<p><b>Features</b> <?php echo htmlentities($result->PackageFetures);?></p>
+				</div>
+				<div class="col-md-3 room-right wow fadeInRight animated" data-wow-delay=".5s">
+					<h5>USD <?php echo htmlentities($result->PackagePrice);?></h5>
+					<a href="package-details.php?pkgid=<?php echo htmlentities($result->PackageId);?>" class="view">Details</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+<?php }} ?>
+
+
+
+		</div>
+	</div>
+</div>
+<!--- /rooms ---->
+
+<!--- /footer-top ---->
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>
+<!-- //write us -->
+</body>
+</html>
