7239ffcba7677cb73f0e4bccad96f80e88ab4354
john albert <johnalbertentereso@yahoo.com>
2021-01-12 03:41:03 +0800

    removed folder rainbowlush

--- sampleweb/rainbowlush/Admin_page/bridge.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username="root";
-$password="";
-$database="rainbowlush";
-mysql_connect("localhost",$username,$password);
-@mysql_select_db($database) or die ("Unable to select database");
-?>
--- sampleweb/rainbowlush/time.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$mydate = strtotime('2018-11-26');
-echo date('F Y', $mydate);
-
-
-?>
